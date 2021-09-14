e47e247bec879e40ac566015f23c7bc4c104cebb
denys.bielkin <dan.98belkin@gmail.com>
2021-09-14 17:23:53 +0300

    Added: • possibility to add a whole playlist to the queue by the same way as a single video

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const {
     token,
 } = require('./config.json');
 const ytdl = require('ytdl-core');
+const ytpl = require('ytpl');
 
 const client = new Discord.Client();
 
@@ -46,7 +47,14 @@ client.on('message', async message => {
 });
 
 async function execute(message, serverQueue) {
+    const videoPattern = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+$/gi;
+    const playlistPattern = /^.*(youtu.be\/|list=)([^#\&\?]*).*/gi;
+
     const args = message.content.split(' ');
+    const isPlaylist = args[1].includes('share')
+    if(isPlaylist) {
+        console.log('ITS PLAYLIST~!');
+    }
 
     const voiceChannel = message.member.voice.channel;
     if (!voiceChannel) return message.channel.send('You need to be in a voice channel to play music!');
@@ -55,24 +63,37 @@ async function execute(message, serverQueue) {
         return message.channel.send('I need the permissions to join and speak in your voice channel!');
     }
     if(args[1]) {
-        const songInfo = await ytdl.getInfo(args[1]);
-        const song = {
-            title: songInfo.videoDetails.title,
-            url: songInfo.videoDetails.video_url,
-        };
+        let songInfo = {}
+        let song = null
+        let mySongList = [];
+        if(isPlaylist) {
+            const songListFromPlaylist = await ytpl(args[1], {pages: 1});
+            for (let i = 0; i < songListFromPlaylist.items.length; i++) {
+                mySongList.push({
+                    title: songListFromPlaylist.items[i].title,
+                    url: songListFromPlaylist.items[i].url,
+                })
+            }
+        } else {
+            songInfo = await ytdl.getInfo(args[1]);
+            song = {
+                title: songInfo.videoDetails.title,
+                url: songInfo.videoDetails.video_url,
+            };
+        }
+        const songs = song ? [song] : mySongList;
+
         if (!serverQueue) {
             const queueContruct = {
                 textChannel: message.channel,
                 voiceChannel: voiceChannel,
                 connection: null,
-                songs: [],
+                songs,
                 volume: 5,
                 playing: true,
             };
 
             queue.set(message.guild.id, queueContruct);
-
-            queueContruct.songs.push(song);
 
             try {
                 var connection = await voiceChannel.join();
@@ -84,7 +105,10 @@ async function execute(message, serverQueue) {
                 return message.channel.send(err);
             }
         } else {
-            serverQueue.songs.push(song);
+            if(isPlaylist) {
+                serverQueue.songs = mySongList
+            }
+            serverQueue.songs = [...serverQueue?.songs, ...songs];
             return message.channel.send(`${song.title} has been added to the queue!`);
         }
     }
